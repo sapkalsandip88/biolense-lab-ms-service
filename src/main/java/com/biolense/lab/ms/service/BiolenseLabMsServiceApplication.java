4ca7926106548419d5fa3e37f92a59d9ca1bda62
sapkalsandip88 <sapkal.sandip88@gmail.com>
2025-02-13 17:39:00 +0530

    Project created first commit

---
+++ src/main/java/com/biolense/lab/ms/service/BiolenseLabMsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.biolense.lab.ms.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiolenseLabMsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiolenseLabMsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biolense/lab/ms/service/BiolenseLabMsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biolense.lab.ms.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiolenseLabMsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
